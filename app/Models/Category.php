8327bb02fdb706f6bbffc6d7d022d1e125a1711d
nour-abolila <nouraliabolilanaaltya101@gmail.com>
2025-10-07 18:43:44 +0300

    add new model and controller category and some updates

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['name', 'image'];
+
+    // الاقسام ممكن تكون بدخلها منتجات العلاقات بينهم
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    // دى دالة كلين كود بستدعيها افضل فى الكنترولر
+    public static function getAllCategories()
+    {
+        return self::all();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('frontend.welcome');
-});
+Route::get('/', [CategoryController::class , 'index']);
+
+
 Route::get('/products', function () {
     return view('frontend.products');
 });
